a7449053c12d8de7e1de7aa0267eb4e5e968d04b
Vijay Stroup <vijaystroup@gmail.com>
2020-10-06 17:55:36 -0400

    terms

--- main.py
+++ main.py
@@ -7,5 +7,10 @@ def home():
     return render_template('home.html', projects=projects, timeline=timeline, tools=tools)
 
 
+@app.route('/terms')
+def terms():
+    return render_template('terms.html')
+
+
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
