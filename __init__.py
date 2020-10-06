2a8b8de2e6b8e2cba4bc9f11569cf93d0454b0fa
Vijay Stroup <vijaystroup@gmail.com>
2020-10-05 20:30:30 -0400

    google api to get resume

--- __init__.py
+++ __init__.py
@@ -5,8 +5,11 @@ from flask import Flask
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 
-with open(f'{PATH}/static/projects.json') as f:
+with open(f'{PATH}/static/json/projects.json') as f:
     projects = json.loads(f.read())
+
+with open(f'{PATH}/static/json/timeline.json') as f:
+    timeline = json.loads(f.read())
 
 tools = os.listdir(f'{PATH}/static/images/tools')
 
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from __init__ import *
 
 @app.route('/')
 def home():
-    return render_template('home.html', projects=projects, tools=tools)
+    return render_template('home.html', projects=projects, timeline=timeline, tools=tools)
 
 
 if __name__ == '__main__':
