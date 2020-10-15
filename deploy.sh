2e12e4e6c26a7d46400415b278460795ac53ea36
Vijay Stroup <vijaystroup@gmail.com>
2020-10-15 19:30:10 -0400

    :pencil2: corrected order

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,14 @@
 
 # this script is called by GitHub Actions to auto-deploy an update
 
+docker stack rm website
+
 # build image
 printf "\n\nBuilding image\n==============\n"
-docker image rm -f website
+docker image rm website
 docker build -t website .
 sleep 3 # sleep for 3 seconds to make sure image is done completely
 
 # deploy stack
 printf "\n\nDeploying stack\n===============\n"
-docker stack rm website
 docker stack deploy -c docker-compose.traefik.yml website
