1e17f523f8ac1f8b1859a9965685ca6544803aae
Vijay Stroup <vijaystroup@gmail.com>
2022-08-21 18:46:52 -0400

    remove trailing slash and apply custom tailwind base styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import preact from '@astrojs/preact'
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind({ config: { applyBaseStyles: false } }), preact()],
+  site: 'https://vijaystroup.com',
+  trailingSlash: 'never'
 })
